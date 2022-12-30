f0ffee53e26ff8bf5d3f6f1eea64f1bcb45fd8e9
tausifabid12 <tasifabid12@gmail.com>
2022-12-30 12:18:18 +0600

    env

--- Components/AddNewTaskFrom/AddNewTaskFrom.js
+++ Components/AddNewTaskFrom/AddNewTaskFrom.js
@@ -24,11 +24,12 @@ const AddNewTaskFrom = () => {
   const handleTaskAdd = (data) => {
     setLoading(true);
     const { priority, decs, taskName } = data;
+
     const image = data.image[0];
     const formData = new FormData();
     formData.append("image", image);
-    // const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMAGEBB_API_KEY}`;
-    const url = `https://api.imgbb.com/1/upload?key=bc8a2458a554fd7f0397c6b63fa4e6e4`;
+    const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMAGEBB_AP_KEY}`;
+
     fetch(url, {
       method: "POST",
       body: formData,
